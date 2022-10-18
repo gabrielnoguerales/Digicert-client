e795716147edddb90d568a5096b1de119f3fec22
Gabriel Noguerales Elviro <gabriel.nogueraleselviro@telefonica.com>
2022-10-18 11:49:46 +0200

    Domain Info

--- src/common/base_connector_classes.py
+++ src/common/base_connector_classes.py
@@ -26,29 +26,6 @@ class BaseConnector(ABC):
             self.session = sessions.BaseUrlSession(base_url=self.base_uri)
             self.session = requests_retry_session(session=self.session)
 
-        @login_required
-        def _get(self, url, **kwargs) -> dict:
-            """
-            Devuelve un get, por si hay algun metodo que no tenemos, podemos usar este
-            Inserta automaticamente el api_key en los "params" del GET
-
-            :param url: url sin la base, donde se encuenta la accion
-            :return:
-            """
-            if not kwargs:
-                kwargs = dict()
-            if "api_key" not in kwargs:
-                kwargs["api_key"] = self.api_key
-
-            response = self.session.get(url=url, params=kwargs)
-            response = Response(response)()
-            if response.error:
-                raise ConnectorException(
-                    "Ha habido un problema: {}".format(response.error_msg)
-                )
-
-            return response.data
-
 
 class BaseConnectorWithVersion(BaseConnector):
     def __init__(self, version, **kwargs):
--- src/digicert_client.py
+++ src/digicert_client.py
@@ -1,7 +1,8 @@
 from requests_toolbelt import sessions
 
 from src.common.base_connector_classes import BaseConnectorWithVersion
-from src.common.wrappers import url_required
+from src.common.exceptions import ConnectorException
+from src.common.wrappers import url_required, login_required
 from src.response import Response
 
 
@@ -11,19 +12,53 @@ class DigicertConnector(BaseConnectorWithVersion):
         super().__init__(version=version, host='{}/{}/'.format(host, version), session=session)
         self.api_key = None
 
+    @login_required
+    def _get(self, url, **kwargs) -> dict:
+        """
+        Devuelve un get, por si hay algun metodo que no tenemos, podemos usar este
+        Inserta automaticamente el api_key en los "params" del GET
+
+        :param url: url sin la base, donde se encuenta la accion
+        :return:
+        """
+        if not kwargs:
+            kwargs = dict()
+        if "api_key" not in kwargs:
+            kwargs["X-DC-DEVKEY"] = self.api_key
+
+        response = self.session.get(url=url, headers=kwargs)
+        response = Response(response)()
+        if response.error:
+            raise ConnectorException(
+                "Ha habido un problema: {}".format(response.error_msg)
+            )
+
+        return response.data
+
     @url_required
     def set_api_token(self, token, url='account') -> None:
         """
-        Permite a un usuario autentificarse usando un API_TOKEN.
+        Allows a user to set the token for the client.
 
         :param url: url without base url
         :param token: API_TOKEN
         :return: keeps api_key in class
         """
-        # no usa el metodo _get, este metodo mete el api_key, y aqui estamos tratando de recuperarlo
-        response = Response(self.session.get(url=url, headers={'Content-Type': "application/json","X-DC-DEVKEY": token}))()
+        # We test the token against the account endpoint before adding it into the class
+        response = Response(
+            self.session.get(url=url, headers={'Content-Type': "application/json", "X-DC-DEVKEY": token}))()
 
         if response.error:
             raise AttributeError('Not able to login in: {}'.format(response.error_msg))
         self.logger.debug('Welcome {}'.format(response.data['rep_name']))
         self.api_key = token
+
+    def get_domain(self, domain_id, url='domain/{}') -> dict:
+        """
+        Given a domin id returns the info of that domain
+
+        :param domain_id: Id of the domain to lookup
+        :param url: url without base
+        :return:
+        """
+        return self._get(url=url.format(domain_id))
